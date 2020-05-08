3057a1ac7bf81e77c3383e50c9c3be486cc08cb4
DaniGTA <danigta2020@gmail.com>
2020-05-09 00:40:21 +0200

    GITHUB WORKSPACE

--- .github/scripts/decrypt_api_keys.sh
+++ .github/scripts/decrypt_api_keys.sh
@@ -6,8 +6,8 @@ gpg --quiet --batch --yes --decrypt --passphrase="$1" --output $GITHUB_WORKSPACE
 
 gpg --quiet --batch --yes --decrypt --passphrase="$1" --output $GITHUB_WORKSPACE/src/keys/api/Simkl.json $GITHUB_WORKSPACE/src/keys/api/Simkl.json.gpg
 
-gpg --quiet --batch --yes --decrypt --passphrase="$1" --output ./src/keys/api/Trakt.json ./src/keys/api/Trakt.json.gpg
+gpg --quiet --batch --yes --decrypt --passphrase="$1" --output $GITHUB_WORKSPACE/src/keys/api/Trakt.json $GITHUB_WORKSPACE/src/keys/api/Trakt.json.gpg
 
-gpg --quiet --batch --yes --decrypt --passphrase="$1" --output ../../src/keys/api/tvdb.json ../../src/keys/api/tvdb.json.gpg
+gpg --quiet --batch --yes --decrypt --passphrase="$1" --output $GITHUB_WORKSPACE/src/keys/api/tvdb.json $GITHUB_WORKSPACE/src/keys/api/tvdb.json.gpg
 
-gpg --quiet --batch --yes --decrypt --passphrase="$1" --output src/keys/api/AniList.json src/keys/api/AniList.json.gpg
+gpg --quiet --batch --yes --decrypt --passphrase="$1" --output $GITHUB_WORKSPACE/src/keys/api/AniList.json $GITHUB_WORKSPACE/src/keys/api/AniList.json.gpg
