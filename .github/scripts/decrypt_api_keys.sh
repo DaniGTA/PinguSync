d207c9c7e48e10a7ceed03bc90153ce9af9d2f44
DaniGTA <danigta2020@gmail.com>
2020-05-09 00:05:26 +0200

    change path

--- .github/scripts/decrypt_api_keys.sh
+++ .github/scripts/decrypt_api_keys.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-cd ../../
 # --batch to prevent interactive command --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$1" --output $HOME/src/keys/api/omdb.json $HOME/src/keys/api/omdb.json.gpg
 
