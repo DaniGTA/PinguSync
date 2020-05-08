862080cb6b0246e58d49762e90d40a11b7991bf5
DaniGTA <danigta2020@gmail.com>
2020-05-08 23:49:35 +0200

    Refresh API_KEY_SAFE

--- .github/scripts/decrypt_api_keys.sh
+++ .github/scripts/decrypt_api_keys.sh
@@ -1,17 +1,13 @@
 #!/bin/sh
 
+cd ../../
 # --batch to prevent interactive command --yes to assume "yes" for questions
-gpg --quiet --batch --yes --decrypt --passphrase="$LARGE_SECRET_PASSPHRASE" \
---output ./src/keys/api/omdb.json ./src/keys/api/omdb.json.gpg
+gpg --quiet --batch --yes --decrypt --passphrase="$1" --output ./src/keys/api/omdb.json ./src/keys/api/omdb.json.gpg
 
-gpg --quiet --batch --yes --decrypt --passphrase="$LARGE_SECRET_PASSPHRASE" \
---output ./src/keys/api/Simkl.json ./src/keys/api/Simkl.json.gpg
+gpg --quiet --batch --yes --decrypt --passphrase="$1" --output ./src/keys/api/Simkl.json ./src/keys/api/Simkl.json.gpg
 
-gpg --quiet --batch --yes --decrypt --passphrase="$LARGE_SECRET_PASSPHRASE" \
---output ./src/keys/api/Trakt.json ./src/keys/api/Trakt.json.gpg
+gpg --quiet --batch --yes --decrypt --passphrase="$1" --output ./src/keys/api/Trakt.json ./src/keys/api/Trakt.json.gpg
 
-gpg --quiet --batch --yes --decrypt --passphrase="$LARGE_SECRET_PASSPHRASE" \
---output ./src/keys/api/tvdb.json ./src/keys/api/tvdb.json.gpg
+gpg --quiet --batch --yes --decrypt --passphrase="$1" --output ./src/keys/api/tvdb.json ./src/keys/api/tvdb.json.gpg
 
-gpg --quiet --batch --yes --decrypt --passphrase="$LARGE_SECRET_PASSPHRASE" \
---output ./src/keys/api/AniList.json ./src/keys/api/AniList.json.gpg
+gpg --quiet --batch --yes --decrypt --passphrase="$1" --output src/keys/api/AniList.json ./src/keys/api/AniList.json.gpg
