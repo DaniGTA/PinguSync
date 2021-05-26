c134339d0b2ec118f20d00c4b9630c2a551661b1
DaniGTA <danigta2020@gmail.com>
2021-05-26 22:30:32 +0200

    fix github decrypt api keys

--- .github/scripts/decrypt_api_keys.sh
+++ .github/scripts/decrypt_api_keys.sh
@@ -2,12 +2,12 @@
 
 cd ../../
 # --batch to prevent interactive command --yes to assume "yes" for questions
-gpg --quiet --batch --yes --decrypt --passphrase="$1" --output $GITHUB_WORKSPACE/src/keys/api/omdb.json $GITHUB_WORKSPACE/src/keys/api/omdb.json.gpg
+gpg --quiet --batch --yes --decrypt --passphrase="$1" --output $GITHUB_WORKSPACE/src/keys/api/OMDbProvider.json $GITHUB_WORKSPACE/src/keys/api/OMDbProvider.json.gpg
 
-gpg --quiet --batch --yes --decrypt --passphrase="$1" --output $GITHUB_WORKSPACE/src/keys/api/Simkl.json $GITHUB_WORKSPACE/src/keys/api/Simkl.json.gpg
+gpg --quiet --batch --yes --decrypt --passphrase="$1" --output $GITHUB_WORKSPACE/src/keys/api/SimklProvider.json $GITHUB_WORKSPACE/src/keys/api/SimklProvider.json.gpg
 
-gpg --quiet --batch --yes --decrypt --passphrase="$1" --output $GITHUB_WORKSPACE/src/keys/api/Trakt.json $GITHUB_WORKSPACE/src/keys/api/Trakt.json.gpg
+gpg --quiet --batch --yes --decrypt --passphrase="$1" --output $GITHUB_WORKSPACE/src/keys/api/TraktProvider.json $GITHUB_WORKSPACE/src/keys/api/TraktProvider.json.gpg
 
-gpg --quiet --batch --yes --decrypt --passphrase="$1" --output $GITHUB_WORKSPACE/src/keys/api/tvdb.json $GITHUB_WORKSPACE/src/keys/api/tvdb.json.gpg
+gpg --quiet --batch --yes --decrypt --passphrase="$1" --output $GITHUB_WORKSPACE/src/keys/api/TVDBProvider.json $GITHUB_WORKSPACE/src/keys/api/TVDBProvider.json.gpg
 
-gpg --quiet --batch --yes --decrypt --passphrase="$1" --output $GITHUB_WORKSPACE/src/keys/api/AniList.json $GITHUB_WORKSPACE/src/keys/api/AniList.json.gpg
+gpg --quiet --batch --yes --decrypt --passphrase="$1" --output $GITHUB_WORKSPACE/src/keys/api/AniListProvider.json $GITHUB_WORKSPACE/src/keys/api/AniListProvider.json.gpg
