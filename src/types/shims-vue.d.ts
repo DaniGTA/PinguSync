fad93520cc58f8289ad6377ffcafd6e90282a287
DaniGTA <danigta2020@gmail.com>
2021-09-14 18:01:09 +0200

    change type file location

--- src/types/shims-vue.d.ts
+++ src/types/shims-vue.d.ts
@@ -1,6 +1,5 @@
 declare module '*.vue' {
-  import type { DefineComponent } from 'vue';
-  // eslint-disable-next-line @typescript-eslint/ban-types, @typescript-eslint/no-explicit-any
-  const component: DefineComponent<{}, {}, any>;
-  export default component;
+    import { defineComponent } from 'vue'
+    const component: ReturnType<typeof defineComponent>
+    export default component
 }
